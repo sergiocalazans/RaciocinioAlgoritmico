f4cce57668694a1514a7aaa7130d330320e4e533
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-12 18:48:01 -0300

    atualizacao-cadastroIdades

--- Aula 1/cadastroIdades.py
+++ Aula 1/cadastroIdades.py
@@ -1,22 +1,19 @@
 # Importando a biblioteca Datetime
-
 import datetime
 
 # Definindo as variáveis de Entrada
-
 anoAtual = datetime.date.today().year
 cadastro = {}
 
 # Processamento
-
 while True:
     
-    nome = str(input("Qual o seu nome?")).strip()
-    anoNascimento = int(input("Qual foi o ano que você nasceu?")).strip()
+    nome = str(input("Nome:")).strip()
+    anoNascimento = int(input("Ano de Nascimento:"))
 
     idade = anoAtual - anoNascimento
 
-    cadastro[nome] = anoNascimento
+    cadastro[nome] = (anoNascimento, idade)
 
     continuar = str(input("Há mais pessoas para cadastrar? (sim/não): ")).strip().lower()
 
@@ -24,7 +21,11 @@ while True:
         break
 
 # Saída
+print(f"{'Nome':<10} {'Ano de Nascimento':<20} {'Idade':<6}")
+print("-" * 40)
 
-for chave in cadastro.keys():
-    print(chave)
+for nome, dados in cadastro.items():
+    anoNascimento, idade = dados  
+    print(f"{nome:<10} {anoNascimento:<20} {idade:<6}")
+
  

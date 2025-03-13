a4e78fcbfa164fd384eda0cdcd9a172ccc1fa8cf
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-13 09:16:24 -0300

    Lista de Exercício #1-2

---
+++ Lista de Exercícios #1/Exercico 2.py
@@ -0,0 +1,14 @@
+# Resolução Exercício 2
+
+# Definindo variáveis de Entrada
+
+ano = 2022
+anoDigitado = int(input("Digite o ano de nascimento:"))
+
+# Processamento
+
+idade = ano - anoDigitado
+
+# Saída
+
+print(f"Em {str(ano)}, você fez {idade} anos.")

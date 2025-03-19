93cd57893970722082fb167a9c17b642a6619603
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-19 09:42:41 -0300

    Aula 3/Exercícios/pesoBoxeador

---
+++ Aula 3/Exercícios/pesoBoxeador.py
@@ -0,0 +1,28 @@
+# Exercício 3
+
+# Entrada
+
+pesoDigitado = float(input("Digite o peso: (em Kg, formato: xx.xx) "))
+
+# Processamento
+
+def calculoCategoria(peso):
+
+    resposta = ""
+
+    if peso < 50:
+        resposta = "Palha"
+    elif 50 <= peso < 60:
+        resposta = "Pluma"
+    elif 60 <= peso < 76:
+        resposta = "Leve"
+    elif 76 <= peso < 88:
+        resposta = "Pesado"
+    elif peso > 88:
+        resposta = "Super Pesado"
+
+    return print(f"Sua categoria é: {resposta}")
+
+# Saída
+
+calculoCategoria(pesoDigitado)

0d47086be37d98288a17334e9b54e2b1bb192625
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-26 08:57:57 -0300

    Lista de Exercícios #2-5 (completo)

--- Lista de Exercícios #2/Exercicio 5.py
+++ Lista de Exercícios #2/Exercicio 5.py
@@ -6,3 +6,21 @@ A partir de 100 cópias, o valor de cada fotocópia tirada cai para R$ 0,20.
 Elabore um algoritmo que leia o número de cópias e mostre o valor a pagar pelo serviço.
 '''
 
+# Entrada
+
+numFotocopia = round(int(input("\nDigite a quantidade de fotocópias: ")), 2)
+
+# Processamento
+
+def calculoFotocopias(qtd):
+
+    if qtd < 100:
+        qtdPreco = f"R$ {qtd * 0.25}"
+    else:
+        qtdPreco = f"R$ {qtd * 0.20}"
+
+    return print(f"\nO valor é de {qtdPreco.replace('.', ',')}.\n")
+
+# Saída
+
+calculoFotocopias(numFotocopia)

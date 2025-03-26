0c7994cbbc1087f3cb3ff95421ce4427c3fddf1b
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-26 08:47:01 -0300

    Lista de Exercícios #2-3 (completo)

--- Lista de Exercícios #2/Exercício 3.py
+++ Lista de Exercícios #2/Exercício 3.py
@@ -5,3 +5,28 @@
 mostre sua raiz quadrada (informe “Valor inválido” para números negativos).
 '''
 
+# Importando a biblioteca Math
+
+import math
+
+# Entrada
+
+while True:
+
+    resposta = ""
+    num = int(input("\nDigite um número inteiro: "))
+
+    if num < 0:
+        resposta = print("Valor inválido, digite um número inteiro")
+        continue
+    else:
+        resposta = print("Valor válido.")
+        break
+
+# Processamento:
+
+raizNum = round(math.sqrt(num), 2) # Cálculo da raíz quadrada
+
+# Saída
+
+print(f"A raíz quadrada do número é: {raizNum}.")

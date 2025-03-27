a9a0fc76e61447755857d5672b3f5cd7d111d30c
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-27 09:34:35 -0300

    Lista de Exercícios #3-8 (completo)

--- Listas de Exercícios/#3/Exercicio 8.py
+++ Listas de Exercícios/#3/Exercicio 8.py
@@ -6,3 +6,20 @@ Mostre então qual o valor da soma e da média aritmética do conjunto.
 
 '''
 
+# Entrada
+
+# Solicita dez números ao usuário e os armazena em uma lista
+numeros = [int(input("Digite um número: ")) for _ in range(10)]
+
+# Processamento
+
+def somaMedia(conjunto):
+
+    soma = sum(conjunto)
+    media = soma / len(conjunto)
+    
+    return print(f"\nA soma do conjunto é {soma} e a média aritmética é {media}.\n")
+
+# Saída
+
+somaMedia(numeros)

03a0bb5d05f4d650bf00f6bfdf78b13d0ade20af
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-27 08:30:36 -0300

    atualizacao desafios

--- Desafios/27-03-2025.py
+++ Desafios/27-03-2025.py
@@ -6,11 +6,11 @@ c2 = 0
 
 while c <= 20:
 
-    if c % 3 == 0:
+    if c % 3 == 0: # Contar os múltiplos de 3
         s += c
         c1 += 1
 
-    elif c % 2 == 0:
+    elif c % 2 == 0: # Contar os múltiplos de 2
         c2 += 1
     
     c += 1

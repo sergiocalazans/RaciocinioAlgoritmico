18f4973ec916c37dede6b40f4f135c38dc441596
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-27 08:58:58 -0300

    Lista de Exercícios #3-5 (completo)

--- Listas de Exercícios/#3/Exercicio 5.py
+++ Listas de Exercícios/#3/Exercicio 5.py
@@ -4,3 +4,21 @@
 5.	Imprima os números ímpares de 1 até n, sendo n fornecido pelo usuário.
 '''
 
+# Entrada 
+
+intervaloMax = int(input("Digite o número máximo do intervalo: "))
+contador = 1
+listaNum = []
+
+# Processamento
+
+while contador <= intervaloMax:
+
+    if contador % 2 != 0:
+        listaNum += [contador]
+    
+    contador += 1
+
+# Saída
+
+print(f"\nNo intervalo de 1 até {intervaloMax}, esses são os números ímpares:\n \n{listaNum}\n")

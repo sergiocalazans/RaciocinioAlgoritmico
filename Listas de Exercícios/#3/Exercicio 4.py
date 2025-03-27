0bd390fcdf50b33e923242f38be3a02bb794b711
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-03-27 08:51:51 -0300

    Lista de Exercícios #3-4 (completo)

--- Listas de Exercícios/#3/Exercicio 4.py
+++ Listas de Exercícios/#3/Exercicio 4.py
@@ -3,3 +3,20 @@
 '''
 4.	Imprima os números múltiplos de 4 existentes no intervalo aberto de 1 a 100.
 '''
+
+# Entrada
+contador = 1
+listaNum = []
+
+# Processamento
+
+while contador <= 100:
+
+    if contador % 4 == 0:
+        listaNum += [contador]
+    
+    contador += 1
+
+# Saída
+
+print(f"\n{listaNum}\n")

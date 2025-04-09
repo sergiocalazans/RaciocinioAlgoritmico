ce9f73f2bda98209d790071a632f3b7dae236034
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-04-09 17:48:46 -0300

    Pasta Lista de Exercícios/#3.1/Exercicio 1.py

---
+++ Listas de Exercícios/#3.1/Exercicio 1.py
@@ -0,0 +1,53 @@
+# Exercício 1
+
+'''
+1.	Ler do teclado uma quantidade de números inteiros a serem lidos e calcular:
+a) a soma total entre eles
+b) a soma dos que forem pares. 
+c) a soma dos que forem ímpares
+d) a amplitude amostral considerando todos os números lidos (diferença entre o maior e o menor)
+'''
+
+# ENTRADA
+
+list_num = []
+
+while True:
+    num = int(input("\nDigite um número inteiro: "))
+
+    list_num += [num]
+
+    continuar = int(input("Deseja digitar mais um número? (1-sim ou 2-não)"))
+
+    if continuar == 2:
+        break
+
+print(f"\n{list_num}")
+
+# PROCESSAMENTO
+
+def calculos(lista):
+
+    soma = sum(lista)
+    soma_pares = []
+    soma_impares = []
+
+    for num in lista:
+
+        if num % 2 == 0:
+            soma_pares += [num]
+        else:
+            soma_impares += [num]
+    
+    soma_pares = sum(soma_pares)
+    soma_impares = sum(soma_impares) 
+
+    amp_amostral = list_num[-1] - list_num[0]
+
+    print(f"\nA soma total dos números é: {soma};")
+    print(f"A soma dos pares é {soma_pares} e dos ímpares é {soma_impares};")
+    print(f"Por fim, a amplitude amostral dos números é {amp_amostral}.")
+
+# SAÍDA
+
+calculos(list_num)

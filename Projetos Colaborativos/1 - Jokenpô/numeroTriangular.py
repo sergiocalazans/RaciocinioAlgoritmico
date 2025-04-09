b0fb0036b1163050314ff24efcdf8f41fd65b4f6
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-04-09 16:13:45 -0300

    k

--- Projetos Colaborativos/1 - Jokenpô/numeroTriangular.py
+++ Projetos Colaborativos/1 - Jokenpô/numeroTriangular.py
@@ -1,18 +1,22 @@
-num = int(input("Digite um número: "))
-
-x = 0
+num = int(input("\nDigite um número: "))
+x = 1
 triangular = 0
+list_num_triangulares = []
 
 while True:
     triangular = (x * (x + 1)) // 2
     
+    list_num_triangulares += [triangular]
+
     if triangular == num:
-        print("é triangular")
+        print("\né triangular")
         break
     elif triangular > num:
-        print("não é triangular")
+        print("\nnão é triangular")
         break
 
     x += 1
+
+print(f"\n{list_num_triangulares}")
 
     

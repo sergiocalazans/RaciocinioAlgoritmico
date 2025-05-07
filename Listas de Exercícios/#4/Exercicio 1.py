5d6002e4bba79155c022b86c0dd7ea01c4ff59b4
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-05-07 08:45:00 -0300

    Lista de Exercícios #4-1 (completo)

---
+++ Listas de Exercícios/#4/Exercicio 1.py
@@ -0,0 +1,29 @@
+# Exercício 1
+
+'''
+1.	Construa a tabela de multiplicação de 1 a 10. (Ex: 1x1 = 1, 1x2=2, 10x10 =100)
+'''
+
+from tabulate import tabulate # Biblioteca para criar tabelas no terminal
+
+# Entrada
+
+tabela = [["Mutiplicação", "Resultado"]]
+contador = 0
+
+# Processamento
+
+def calcular_tabela(tabela):
+
+    for valor in range(1, 11):
+
+        tabela.append([f"1x{valor}", (1 * valor)])
+
+    print(tabulate(tabela, headers="firstrow", tablefmt="fancy_grid"))
+
+# Saída
+
+calcular_tabela(tabela)
+
+
+

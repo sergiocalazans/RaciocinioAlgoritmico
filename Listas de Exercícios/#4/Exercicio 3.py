6f949cb3e0ba8b76d3f76028967ba93f13bfa195
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-05-07 09:02:06 -0300

    Lista de Exercícios #4-3 (completo)

---
+++ Listas de Exercícios/#4/Exercicio 3.py
@@ -0,0 +1,26 @@
+# Exercício 3
+
+'''
+3.	Leia três números do teclado e verificar se o primeiro é maior que a soma dos outros dois.
+'''
+
+# Entrada
+
+contador = 1
+lista = []
+
+while contador <= 3:
+
+    num = int(input("\nDigite um número: "))
+    lista.append(num)
+    contador += 1
+
+# Processamento e Saída
+
+if lista[0] > sum(lista[-2:]):
+
+    print("\nO primeiro número digitado, é maior do que a soma dos últimos números.\n")
+
+else:
+
+    print("\nO primeiro número digitado, não é maior do que a soma dos últimos números.\n")

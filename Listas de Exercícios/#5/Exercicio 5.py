16168c2f09b374b90d91d8e3e4a0387823c486a8
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-05-21 09:34:28 -0300

    Lista de Exercícios #5-5 (completo)

--- Listas de Exercícios/#5/Exercicio 5.py
+++ Listas de Exercícios/#5/Exercicio 5.py
@@ -8,66 +8,56 @@ mostre a matriz apenas com os valores que estão na parte hachurada e mostre a s
 
 import random as rd
 
-aMatriz = []
-bMatriz = []
-cMatriz = []
-dMatriz = []
+qtdLinhas = 5
+qtdColunas = 5
 
-def criarMatriz(nMatriz): 
+def criar_matriz(linhas, colunas):
+    return [[rd.randint(10, 99) for _ in range(colunas)] for _ in range(linhas)]
 
-    j = 1
-    i = 0
-    matriz_original = matriz(5, 5)
-
-    while len(nMatriz) <= 5:
-
-        for linha in matriz_original:
-                
-                lista = []
-
-                for elemento in linha:
-
-                    if elemento == linha[2]:
-                        lista.append(str(elemento))
-                    else:
-                        lista.append(str(0).zfill(2))
-                    
-                nMatriz.append(lista)
-                j += 1
-
-    imprimirMatriz(nMatriz)
-
-def matriz(qtdColunas, qtdLinhas):
-     
-    matriz = []
-    i = 0
-
-    while len(matriz) <= qtdLinhas:
-
-        lista = []
-
-        for _ in range(qtdColunas):
-                num = rd.randrange(10, 99)
-                lista.append(num)
-
-        matriz.append(lista)
-    
-    return matriz
-
-def imprimirMatriz(matriz):
-    
+def imprimir_matriz(matriz):
     for linha in matriz:
         for elemento in linha:
-            print(elemento, end=' ')
+            print(str(elemento).zfill(2), end=' ')
         print()
-     
-print("\nMatriz: A")
-criarMatriz(aMatriz)
+    print()
 
+def filtrar_matriz(matriz, tipo):
+    filtrada = []
+    soma = 0
+    for i in range(len(matriz)):
+        linha = []
+        for j in range(len(matriz[i])):
+            valor = matriz[i][j]
+            if tipo == 'a' and (i == 2 or j == 2):  # cruz
+                linha.append(valor)
+                soma += valor
+            elif tipo == 'b' and (i == 0 or i == 4 or j == 0 or j == 4):  # moldura
+                linha.append(valor)
+                soma += valor
+            elif tipo == 'c' and (i == j or i + j == 4):  # diagonais
+                linha.append(valor)
+                soma += valor
+            elif tipo == 'd' and (i + j) % 2 == 0:  # tabuleiro de xadrez
+                linha.append(valor)
+                soma += valor
+            else:
+                linha.append(0)
+        filtrada.append(linha)
+    return filtrada, soma
+
+def matriz(tipo):
     
+    global matriz_original
 
-        
-          
+    filtrada, soma = filtrar_matriz(matriz_original, tipo)
+    print(f"Matriz {tipo.upper()}:")
+    imprimir_matriz(filtrada)
 
+    print(f"Soma dos elementos hachurados: {soma}\n{'-'*40}")
 
+matriz_original = criar_matriz(qtdLinhas, qtdColunas)
 
+matriz('a')
+matriz('b')
+matriz('c')
+matriz('d')
